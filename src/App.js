146f88118b96178f47ab001d17420b5eaf271919
ObedQ <olotu@st.ug.edu.gh>
2024-02-20 10:56:16 +0000

    Add Task Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -34,14 +35,21 @@ const toggleReminder = (id) => {
     !task.reminder} : task))
 }
 
+//Add task
+const addTask = (task) => {
+  const id = Math.floor (Math.random() * 10000) + 1
+  const newTask = { id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 //Delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
   return (
     <div className="container">
-      <Header/>
-      <AddTask/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>: 'No Task To Show'}
     </div>
   );
